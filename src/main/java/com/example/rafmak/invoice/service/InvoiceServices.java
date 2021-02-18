5cf09480cb08ca061366d612dee6efad7b2ad3c6
Marjan Karafiloski <73258028+Geca13@users.noreply.github.com>
2021-02-18 01:15:42 +0100

    small change on invoice print method

--- src/main/java/com/example/rafmak/invoice/service/InvoiceServices.java
+++ src/main/java/com/example/rafmak/invoice/service/InvoiceServices.java
@@ -11,10 +11,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+
 import com.example.rafmak.billing.entity.BillingProducts;
 import com.example.rafmak.billing.repository.BillingProductsRepository;
 import com.example.rafmak.finance.entity.Payment;
 import com.example.rafmak.finance.repository.PaymentRepository;
+import com.example.rafmak.invoice.controller.emptyInvoiceException;
 import com.example.rafmak.invoice.entity.Company;
 import com.example.rafmak.invoice.entity.ExpiredDateInvoices;
 import com.example.rafmak.invoice.entity.Invoice;
@@ -277,6 +281,9 @@ public class InvoiceServices {
     	
     	companyRepository.save(company1);
     }
+    
+    
+  
     
     public void exportToPdf(HttpServletResponse response, Integer id) throws DocumentException, IOException {
     	
